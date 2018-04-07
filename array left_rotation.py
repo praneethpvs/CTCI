b5b9dc0a20ced86da973dc68ddec0fbd96323e4e
praneethpvs <praneeth.bobby20@gmail.com>
2018-04-07 02:02:34 -0400

    Updated the code to an efficient solution.

--- array left_rotation.py
+++ array left_rotation.py
@@ -1,30 +1,41 @@
 # CTCI
-def array_left_rotation(a, n, k):
-    output = [None] * n
+from pprint import pprint
+
+
+class UndefinedArrayLength(Exception):
+    def __init__(self, ipstr):
+        self.ipstr = ipstr
+
+
+class UndefinedRotationNo(Exception):
+    def __init__(self, ipstr):
+        self.ipstr = ipstr
+
+
+def array_left_rotation(a, n):
+    k = len(a)
     try:
-        if 1 <= n <= 100000 and 1 <= k <= n:
-            old_pos = 0
-            for i in range(len(a)):
-                if i == 0:
-                    new_pos = (n + i) % k
-                    if new_pos >= len(a):
-                        new_pos -= 1
-                    output[new_pos] = a[i]
-                else:
-                    new_pos = (n + i) % k
-                    if new_pos <= old_pos:
-                        new_pos += k
-                    if new_pos >= len(a):
-                        new_pos -= len(a)
-                    old_pos = new_pos
-                    output[new_pos] = a[i]
-                i += 1
-            return output
+        if 1 <= k <= 100000 and 1 <= n <= k:
+            s = n % k
+            shifted_array = a[s:] + a[:s]
+            return shifted_array
+        elif k < 1 or k > 100000:
+            raise UndefinedArrayLength("Length of array not in mentioned limits.")
+        elif n < 1 or n > k:
+            raise UndefinedRotationNo("Number of rotation times is not within the specified limit.")
+    except UndefinedArrayLength as err:
+        print err.ipstr
+    except UndefinedRotationNo as err:
+        print err.ipstr
+
+
+if __name__ == "__main__":
+    list_a = map(int, raw_input("Enter list elements separated by space in a single line: ").strip().split(' '))
+    k_times = int(raw_input("Enter the no.of left rotations to be performed: "))
+    try:
+        print "input list: {}".format(list_a)
+        answer = array_left_rotation(list_a, k_times)
+        op_list = ' '.join(map(str, answer))
+        print "List after performing {} left rotations : {}".format(k_times, op_list)
     except Exception as e:
         print e
-
-
-no, k_times = map(int, raw_input().strip().split(' '))
-list_a = map(int, raw_input().strip().split(' '))
-answer = array_left_rotation(list_a, no, k_times)
-print ' '.join(map(str, answer))
